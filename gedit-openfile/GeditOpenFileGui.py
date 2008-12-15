6eee12ffca37d628e8442e4a78f227931b596c8d
Vitaly Babiy <vbabiy86@gmail.com>
2008-12-15 17:26:07 -0500

    Formating Of File

--- gedit-openfile/GeditOpenFileGui.py
+++ gedit-openfile/GeditOpenFileGui.py
@@ -33,7 +33,7 @@ class GeditOpenFileGui(object):
         self._plugin_window = self._builder.get_object("gedit_openfiles_window")
         self._plugin_window.set_transient_for(self._window)
         self._notebook = self._builder.get_object('notebook')
-        
+
         # Callbacks
         self._plugin_window.connect("key-release-event", self._on_window_release)
         self._plugin_window.connect("delete_event", self._plugin_window_delete_event)
@@ -71,7 +71,7 @@ class GeditOpenFileGui(object):
         building_data_spinner.set_from_animation(gtk.gdk.PixbufAnimation(
             os.path.join(os.path.dirname(__file__), "gui", "progress.gif")))
         self._building_data_spinner_box = self._builder.get_object('spinner_box')
-        
+
         # Setup Configuration Tab
         self._notebook = self._builder.get_object("notebook")
         self._file_browser_checkbox = self._builder.get_object("file_browser_checkbox")
@@ -79,28 +79,28 @@ class GeditOpenFileGui(object):
         self._open_root_hbox = self._builder.get_object("open_root_hbox")
         self._open_root_path = self._builder.get_object("open_root_path")
         self._config_ignore_input = self._builder.get_object("config_ignore_input")
-        
+
         self._reset_config()
-        
+
         # Connect the OK Button the config tab
         self._builder.get_object("config_save_button").connect("clicked", self._save_config_event)
         self._builder.get_object("config_cancel_button").connect("clicked", self._cancel_config_event)
         self._builder.get_object("config_refresh_button").connect("clicked", self._refresh_data)
-        
+
         use_file_browser = self._config.get_value("USE_FILEBROWSER")
         if use_file_browser == True or use_file_browser == None: # Defualt
             self._open_root_hbox.set_sensitive(False)
         else:
             self._open_root_hbox.set_sensitive(True)
-        
+
         # Set encoding
         self._encoding = gedit.encoding_get_current()
         self._insert_menu()
-    
+
     def _refresh_data(self, event):
         self._file_monitor.refresh_database()
         self._plugin_window.hide()
-        
+
     def _reset_config(self):
         if self._config.get_value("USE_FILEBROWSER"):
             self._file_browser_checkbox.set_active(True)
@@ -109,11 +109,11 @@ class GeditOpenFileGui(object):
         log.debug("IGNORE_FILE_FILETYPES = " + str(self._config.get_value("IGNORE_FILE_FILETYPES")))
         log.debug("TYPE = " + str(type(self._config.get_value("IGNORE_FILE_FILETYPES"))))
         self._config_ignore_input.set_text(", ".join(self._config.get_value("IGNORE_FILE_FILETYPES")))
-    
+
     def _cancel_config_event(self, event):
         self._reset_config()
-        self._plugin_window.hide()        
-        
+        self._plugin_window.hide()
+
     def _save_config_event(self, event):
         self._config.set_value("USE_FILEBROWSER", self._file_browser_checkbox.get_active())
         ignored_list = [s.strip() for s in self._config_ignore_input.get_text().split(",")]
@@ -122,14 +122,13 @@ class GeditOpenFileGui(object):
         self._file_monitor.set_root_path(self._config.root_path())
         self._file_monitor.refresh_database()
         self._plugin_window.hide()
-    
+
     def _file_browser_checkbox_event(self, widget):
         if widget.get_active():
             self._open_root_hbox.set_sensitive(False)
         else:
             self._open_root_hbox.set_sensitive(True)
-            
-        
+
     def _plugin_window_delete_event(self, window, event):
         """
         Method used to is trigger when the x is click on the window, it will not
@@ -137,25 +136,30 @@ class GeditOpenFileGui(object):
         """
         self._plugin_window.hide()
         return True
-        
+
     def update_ui(self):
         log.error("[GeditOpenFileGui] update_ui METHOD NOT IMPLEMENTED")
 
-    #TODO refactor and reivew code. To make sure its not doing more work then is needed.
     def _insert_menu(self):
+        #TODO refactor and reivew code. To make sure its not doing more work then is needed.
         manager = self._window.get_ui_manager()
-        self._action_group = gtk.ActionGroup( "GeditOpenFileAction" )
-        plugin_menu_action = gtk.Action( name="GeditOpenFileAction", label="Open", tooltip="Gedit Open File(s) tools", stock_id=None )
-        self._action_group.add_action( plugin_menu_action )
-        
-        geditopenfiles_action = gtk.Action( name="GeditOpenFileMenuAction", label="Open File(s)...\t", tooltip="Open a file(s)", stock_id=gtk.STOCK_OPEN )
-        geditopenfiles_action.connect( "activate", lambda a: self.on_geditopen_action())
-        self._action_group.add_action_with_accel( geditopenfiles_action, "<Ctrl><Alt>o" )
-        manager.insert_action_group( self._action_group, 0 )
-        
+        self._action_group = gtk.ActionGroup("GeditOpenFileAction")
+        plugin_menu_action = gtk.Action(name="GeditOpenFileAction",
+            label="Open", tooltip="Gedit Open File(s) tools", stock_id=None)
+        self._action_group.add_action(plugin_menu_action)
+
+        geditopenfiles_action = gtk.Action(name="GeditOpenFileMenuAction",
+            label="Open File(s)...\t", tooltip="Open a file(s)",
+            stock_id=gtk.STOCK_OPEN)
+        geditopenfiles_action.connect("activate",
+            lambda a: self.on_geditopen_action())
+        self._action_group.add_action_with_accel(geditopenfiles_action,
+            "<Ctrl><Alt>o")
+        manager.insert_action_group(self._action_group, 0)
+
         self._ui_id = manager.new_merge_id()
-        
-        manager.add_ui_from_string( menu_str )
+
+        manager.add_ui_from_string(menu_str)
         manager.ensure_update()
 
     def _on_window_release(self, widget, event):
@@ -165,30 +169,31 @@ class GeditOpenFileGui(object):
             log.debug("DOWN WAS PRESSED")
 
     def _on_query_entry(self, widget, event):
-        self._clear_treeveiw() # Remove all 
-        
+        self._clear_treeveiw() # Remove all
+
         input_query = widget.get_text()
         log.debug(" [GeditOpenFileGui] input_query : %s" % input_query)
-        
+
         if input_query:
             # Query database based on input
             results = self._file_monitor.search_for_files(input_query)
             self._insert_into_treeview(results)
-        
+
     def _insert_into_treeview(self, file_list):
         for file in file_list:
             self._liststore.append([file.display_path, file.uri])
-            
+
     def _clear_treeveiw(self):
         self._liststore.clear()
-        
+
     def _open_file(self, uri):
         # Check to make sure file is not allready opened
         tab = self._window.get_tab_from_uri(uri)
         if not tab:
             # if not createa tab.
-            tab = self._window.create_tab_from_uri( uri, self._encoding, 0, False, False )
-        self._window.set_active_tab( tab )
+            tab = self._window.create_tab_from_uri(uri, self._encoding, 0,
+                False, False)
+        self._window.set_active_tab(tab)
 
     def _foreach(self, model, path, iter, selected):
         """
@@ -202,14 +207,14 @@ class GeditOpenFileGui(object):
         self._file_list.get_selection().selected_foreach(self._foreach, selected)
         for selected_file in selected:
             # Open File
-            self._open_file ( selected_file )
+            self._open_file(selected_file)
 
         # Hide the window
         self._plugin_window.hide()
 
     def _on_list_mouse(self, widget, event):
         log.error("_on_list_mouse METHOD NOT DEFINED")
-    
+
     def on_geditopen_action(self):
         self._plugin_window.show()
         self._notebook.set_current_page(0) # Set back to the search page
