f10e0826afde338e9f3dcff4d55f5c630469db23
Vitaly Babiy <vbabiy86@gmail.com>
2008-12-15 17:27:59 -0500

    Formating Of File

--- gedit-openfile/GeditOpenFile.py
+++ gedit-openfile/GeditOpenFile.py
@@ -14,22 +14,24 @@ class GeditOpenFile(Plugin):
 
         # Create DB Wrapper and start the thread
         self._db_wrapper = DBWrapper()
-        
+
         self._config = Config()
-        self._file_monitor = FileMonitor(self._db_wrapper, self._config.root_path(), self._config)
+        self._file_monitor = FileMonitor(self._db_wrapper,
+            self._config.root_path(), self._config)
 
-    def _get_instance( self, window ):
-        return window.get_data( self.DATA_TAG )
+    def _get_instance(self, window):
+        return window.get_data(self.DATA_TAG)
 
-    def _set_instance( self, window, instance ):
-        window.set_data( self.DATA_TAG, instance )
+    def _set_instance(self, window, instance):
+        window.set_data(self.DATA_TAG, instance)
 
-    def activate( self, window ):
-        self._set_instance( window, GeditOpenFileGui(self, window, self._file_monitor, self._config))
+    def activate(self, window):
+        self._set_instance(window, GeditOpenFileGui(self, window,
+            self._file_monitor, self._config))
 
-    def deactivate( self, window ):
-        self._get_instance( window ).deactivate()
-        self._set_instance( window, None )
+    def deactivate(self, window):
+        self._get_instance(window).deactivate()
+        self._set_instance(window, None)
 
-    def update_ui( self, window ):
-        self._get_instance( window ).update_ui()
+    def update_ui(self, window):
+        self._get_instance(window).update_ui()
