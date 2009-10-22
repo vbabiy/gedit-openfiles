b411435ac1e5e67f26f37683ebfce4a549a481c1
Vitaly Babiy <vbabiy86@gmail.com>
2009-10-22 09:42:08 -0400

    Updated to support pyinotify 0.8.6

--- gedit-openfiles/FileMonitor.py
+++ gedit-openfiles/FileMonitor.py
@@ -11,12 +11,15 @@ import stat
 import re
 import urllib
 from Logger import log
-from pyinotify import WatchManager, Notifier, ThreadedNotifier, \
-EventsCodes, ProcessEvent
+from pyinotify import WatchManager, Notifier, ThreadedNotifier, EventsCodes, ProcessEvent
 from threading import Thread
 
-EVENT_MASK = EventsCodes.IN_DELETE | EventsCodes.IN_CREATE | \
-EventsCodes.IN_MOVED_TO | EventsCodes.IN_MOVED_FROM # watched events
+try:
+    EVENT_MASK = EventsCodes.IN_DELETE | EventsCodes.IN_CREATE | EventsCodes.IN_MOVED_TO | EventsCodes.IN_MOVED_FROM # watched events
+except AttributeError:
+    # Support for new version of pyinotify
+    from pyinotify import IN_DELETE, IN_CREATE, IN_MOVED_FROM, IN_MOVED_TO
+    EVENT_MASK = IN_DELETE | IN_CREATE | IN_MOVED_TO | IN_MOVED_FROM
 
 
 class FileMonitor(object):
@@ -135,7 +138,7 @@ class WalkDirectoryThread(Thread):
                     # Check to see if it is a dir
                     if not os.path.isdir(os.path.join(path, name)):
                         self._file_monitor.add_file(path, name)
-        print "***** Total files %s *****" % (self._file_monitor._file_count, )
+#        print "***** Total files %s *****" % (self._file_monitor._file_count, )
 
     def _walk_file_system(self, root):
         """
